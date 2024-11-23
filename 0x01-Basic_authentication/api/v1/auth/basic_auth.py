7263c3aa824ed262e8853bbe49a5df9a6645f22f
Abubakar Abdulazeez Usman <arabiusman99@gmail.com>
2024-11-23 01:05:08 +0100

    done

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -43,7 +43,8 @@ class BasicAuth(Auth):
         email, password = decoded_base64_authorization_header.split(':', 1)
         return (email, password)
         
-    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):
+    def user_object_from_credentials(self, user_email: str, user_pwd: str) \
+            -> TypeVar('User'):
         """ Returns the User instance based on his email and passsword."""
         if user_email is None or not isinstance(user_email, str):
             return None
