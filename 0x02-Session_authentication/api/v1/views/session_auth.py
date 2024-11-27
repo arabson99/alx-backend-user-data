1fadf2bfbc34d01769ac6ef0706edc16fccd94dd
Abubakar Abdulazeez Usman <arabiusman99@gmail.com>
2024-11-27 15:27:07 +0100

    pycodestyle

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -37,7 +37,8 @@ def login():
     return make_response(jsonify({"error": "wrong password"}), 401)
 
 
-@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/auth_session/logout',
+                 methods=['DELETE'], strict_slashes=False)
 def logout():
     """ Auth session logou.t"""
     from api.v1.app import auth
