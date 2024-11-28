fae1eb7adc3c4d1b1041a5417e56ae7a41180c30
Abubakar Abdulazeez Usman <arabiusman99@gmail.com>
2024-11-29 00:15:58 +0100

    Basic Flask app Credential validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -30,3 +30,13 @@ class Auth:
             user = self._db.add_user(email, password)
 
         return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validate password for a specific user"""
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(
+                password.encode('utf-8'), user.hashed_password.encode('utf-8')
+            )
+        except NoResultFound:
+            return False
