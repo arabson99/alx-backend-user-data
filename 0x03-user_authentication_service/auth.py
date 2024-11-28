596f3ba301f11b4625ebdedaa3a364e84c55b18b
Abubakar Abdulazeez Usman <arabiusman99@gmail.com>
2024-11-28 23:09:09 +0100

    Task 4. Hash password

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" auth module. """
+
+import bcrypt
+
+
+def _hash_password(password):
+    """ Hashes a password and return the hashed password."""
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
