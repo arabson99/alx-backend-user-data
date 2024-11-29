48ffa37fa2bd3d5d683cca8eea72b24a4f30b6b4
Abubakar Abdulazeez Usman <arabiusman99@gmail.com>
2024-11-29 11:35:56 +0100

    Basic Flask app Log in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -40,13 +40,13 @@ def login():
     if not email or not password:
         abort(401)
 
-    if not AUTH.valid_login(email, password)
-    abort(401)
+    if not AUTH.valid_login(email, password):
+        abort(401)
 
     session_id = AUTH.create_session(email)
     if session_id:
         response = jsonify({"email": email, "message": "logged in"})
-        response.set_cookie("session_id": session_id)
+        response.set_cookie("session_id", session_id)
         return response
 
 
