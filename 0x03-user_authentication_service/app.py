e885f0a159a9aded70c822e2608e5bbc05d2f9af
Abubakar Abdulazeez Usman <arabiusman99@gmail.com>
2024-11-29 13:32:03 +0100

    Update password end-point

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -90,7 +90,7 @@ def update_password():
 
     try:
         AUTH.update_password(reset_token, new_password)
-        return jsonify({"email": email, "message": "Password updated"}), 200
+        return jsonify({"email": email, "message": "OK"}), 200
     except ValueError:
         abort(403)
 
